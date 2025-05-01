a4b5668a33806be5656a7a1d8450e931c4010b7f
teddyand <davidchen1977@live.com>
2025-05-01 16:19:49 +0800

    25 May 1st express

---
+++ netlify/functions/api.ts
@@ -0,0 +1,13 @@
+// YOUR_BASE_DIRECTORY/netlify/functions/api.ts
+
+import express, { Router } from "express";
+import serverless from "serverless-http";
+
+const api = express();
+
+const router = Router();
+router.get("/hello", (req, res) => res.send("Hello Welcome to David's maker World!"));
+
+api.use("/api/", router);
+
+export const handler = serverless(api);
